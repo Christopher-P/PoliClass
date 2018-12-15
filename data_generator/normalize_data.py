b85af97e0871bd48033c63d545739221b1eb0662
christopher-p <christopher.pereyda@gmail.com>
2018-12-14 16:06:11 -0800

    Last commit before submission

--- data_generator/normalize_data.py
+++ data_generator/normalize_data.py
@@ -17,17 +17,22 @@ with open('../data_collector/classified.csv', newline='') as csvfile:
         labels_x.append(int(row[1]))
         labels_y.append(int(row[2]))
 
-# Get average without zeros
-tmp_X = [x for x in labels_x if x != 0]
-avg_X = sum(tmp_X)/len(tmp_X)
-tmp_Y = [x for x in labels_y if x != 0]
-avg_Y = sum(tmp_Y)/len(tmp_Y)
+combined = list(zip(tweets, labels_x, labels_y))
+random.shuffle(combined)
+
+tweets[:], labels_x[:], labels_y[:] = zip(*combined)
 
 # Set zeros to average
 for ind, val in enumerate(labels_x):
     if val == 0:
-        labels_x[ind] = avg_X
-        labels_y[ind] = avg_Y
+        labels_x[ind] = 300
+        labels_y[ind] = 300
+
+# Get average with zeros set to middle (300,300)
+tmp_X = [x for x in labels_x if x != 0]
+avg_X = sum(tmp_X)/len(tmp_X)
+tmp_Y = [x for x in labels_y if x != 0]
+avg_Y = sum(tmp_Y)/len(tmp_Y)
 
 # Normalize values
 #lower, upper = -1, 1
@@ -38,6 +43,8 @@ max_y = max(labels_y)
 
 norm_x = [(x - min_x) / (max_x - min_x) for x in labels_x]
 norm_y = [(x - min_y) / (max_y - min_y) for x in labels_y]
+
+print(len(labels_x))
 
 #print(norm_x)
 #print(norm_y)
